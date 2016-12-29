fa4d95150c5ce51830ddd80fa5eb36d0e907b503
5hirish <shirishkadam35@yahoo.com>
2016-12-29 14:42:01 +0530

    Added RecylerView to TodayFragment

--- app/src/main/java/com/alleviate/antidetention/CreateScheduleActivity.java
+++ app/src/main/java/com/alleviate/antidetention/CreateScheduleActivity.java
@@ -6,6 +6,9 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.TimePicker;
 
 public class CreateScheduleActivity extends AppCompatActivity {
 
@@ -15,6 +18,16 @@ public class CreateScheduleActivity extends AppCompatActivity {
         setContentView(R.layout.activity_create_schedule);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        Spinner day = (Spinner)findViewById(R.id.day);
+        Spinner lecture = (Spinner)findViewById(R.id.lecture);
+        Spinner lecturer = (Spinner)findViewById(R.id.lecturer);
+
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.planets_array, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        day.setAdapter(adapter);
+        lecture.setAdapter(adapter);
+        lecturer.setAdapter(adapter);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
--- app/src/main/java/com/alleviate/antidetention/TodayFragment.java
+++ app/src/main/java/com/alleviate/antidetention/TodayFragment.java
@@ -3,9 +3,15 @@ package com.alleviate.antidetention;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.DefaultItemAnimator;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 
 
 /**
@@ -18,12 +24,29 @@ public class TodayFragment extends Fragment {
         // Required empty public constructor
     }
 
+    RecyclerView.Adapter rvadpter;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
+        View view = inflater.inflate(R.layout.fragment_today, container, false);
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_today, container, false);
+        String movies[] = getResources().getStringArray(R.array.planets_array);
+        final ArrayList mcu_movies = new ArrayList<String>(Arrays.asList(movies));
+
+        RecyclerView rv = (RecyclerView)view.findViewById(R.id.recycler_view_today);
+        rv.addItemDecoration(new DividerItemDecoration(getActivity(), LinearLayoutManager.VERTICAL));
+        rv.setHasFixedSize(true);
+
+        RecyclerView.LayoutManager rvlayoutmanager = new LinearLayoutManager(getActivity());
+        rv.setLayoutManager(rvlayoutmanager);
+
+        rvadpter = new TodayAdapter(getActivity(), mcu_movies);
+        rv.setAdapter(rvadpter);
+
+        rv.setItemAnimator(new DefaultItemAnimator());
+        return view;
     }
 
 }
